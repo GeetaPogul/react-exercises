bc2d17779853c67286d2c91ab631aaaebb167840
GeetaPogul <87964049+GeetaPogul@users.noreply.github.com>
2022-02-18 13:26:57 +0530

    initial commit Added 3 buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import "./styles.css";
+import React, { useState } from "react";
 
 export default function App() {
+  const [fontHeight, setFontHeight] = useState("1em");
+
+  console.log({ fontHeight });
+
+  const changeToFour = () => {
+    setFontHeight("4em");
+  };
+
+  const changeToTwo = () => {
+    setFontHeight("2em");
+  };
+
+  const changeToOne = () => {
+    setFontHeight("1em");
+  };
+
   return (
-    <div className="App">
-      <h1>Hello !</h1>
-      <h2>Hello !</h2>
+    <div>
+      <button onClick={changeToFour}> h1 </button>
+      <button onClick={changeToTwo}> h2 </button>
+      <button onClick={changeToOne}> Back to normal </button>
+
+      <div style={{ fontSize: fontHeight }}>
+        I am a complan girl! {fontHeight}{" "}
+      </div>
     </div>
   );
 }
